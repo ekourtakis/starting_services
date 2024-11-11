f2571f975d8d7b45cba15ca9d4bf8312a0853802
ekourtakis <emmanuel.kourtakis@temple.edu>
2024-11-11 15:55:37 -0500

    TimerService now uses coroutines

--- app/src/main/java/com/example/startingservices/TimerService.kt
+++ app/src/main/java/com/example/startingservices/TimerService.kt
@@ -4,8 +4,14 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class TimerService : Service() {
+    private val serviceScope = CoroutineScope(Dispatchers.IO + Job())
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         startTimer()
         return START_STICKY
@@ -16,16 +22,12 @@ class TimerService : Service() {
     }
 
     private fun startTimer() {
-        Thread {
+        serviceScope.launch {
             for (i in 100 downTo 0) {
-                Log.d("TimerService", "Countdown: $i")
+                Log.d("Timer", i.toString())
                 Thread.sleep(1000) // 1-second delay
             }
             stopSelf()
         }.start()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
     }
 }
